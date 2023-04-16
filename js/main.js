664eeec24faa72feab62e5024b12740c1aa3bfd7
Rafael Moura <123047561+takamasurafael@users.noreply.github.com>
2023-04-15 22:17:27 -0300

    Add files via upload
    Atualizado as funções de adicionar task dentro dos cards dos quadros criados

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const cardItems = document.querySelectorAll(".cardItem");
 const cards = document.querySelectorAll(".card");
 const titleCard = document.querySelectorAll(".titleCard");
 const editOptions = document.querySelectorAll("fa-solid", "fa-pen-to-square");
+const addTaskItem = document.querySelectorAll(".addTaskItem");
 
 let tasks = [];
 
@@ -21,33 +22,47 @@ const addTask = (index) => {
   const i = document.createElement("i");
   i.classList.add("fa-solid", "fa-pen-to-square");
   i.addEventListener("click", () => {
-    // editar modal
     const h4Text0 = prompt("Qual o título da tarefa?") || "sem título";
     h4.innerText = h4Text0;
-    const pText0 = prompt("Qual a tarefa?") || "sem conteúdo";
-    p.innerText = pText0;
   });
 
   const divTaskContentItem = document.createElement("div");
   divTaskContentItem.classList.add("taskContentItem");
 
-  const input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
+  const span = document.createElement("span");
+  span.classList.add("addTaskItem");
 
-  input.addEventListener("change", () => {
-    if (input.checked) {
-      p.style.textDecoration = "line-through";
-      p.style.fontStyle = "italic";
-    } else {
-      p.style.textDecoration = "none";
-      p.style.fontStyle = "normal";
-    }
+  const iPlusAddTaskItem = document.createElement("i");
+  iPlusAddTaskItem.classList.add("fa-solid", "fa-plus");
+
+  iPlusAddTaskItem.addEventListener("click", () => {
+    const divBoxTask = document.createElement("div");
+    divBoxTask.classList.add("divBoxTask");
+
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.setAttribute("id", "input-" + index);
+
+    const p = document.createElement("p");
+    p.setAttribute("id", "p-" + index);
+    const pText0 = p ? prompt("Qual a tarefa?") : "sem texto";
+    p.innerText = pText0;
+
+    divTask.appendChild(divTaskContentItem);
+    divTaskContentItem.appendChild(divBoxTask);
+    divBoxTask.appendChild(input);
+    divBoxTask.appendChild(p);
+
+    input.addEventListener("change", () => {
+      if (input.checked) {
+        p.style.textDecoration = "line-through";
+        p.style.fontStyle = "italic";
+      } else {
+        p.style.textDecoration = "none";
+        p.style.fontStyle = "normal";
+      }
+    });
   });
-
-  const p = document.createElement("p");
-  p.setAttribute("id", "p-" + index);
-  const pTextInner = prompt("Qual a tarefa?") || "sem conteúdo";
-  p.innerText = pTextInner;
 
   let h1Element = cardItems[index].querySelector("h1");
 
@@ -60,8 +75,8 @@ const addTask = (index) => {
   divTask.appendChild(h4);
   divTask.appendChild(i);
   divTask.appendChild(divTaskContentItem);
-  divTaskContentItem.appendChild(input);
-  divTaskContentItem.appendChild(p);
+  divTask.appendChild(span);
+  span.appendChild(iPlusAddTaskItem);
 
   divTask.addEventListener("dragstart", (event) => {
     event.dataTransfer.setData("text/plain", event.target.id);
